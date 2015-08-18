fc7a7e4df1cebae6b1a9bc4d9a94d9e60ecbce24
Pavel Bocharov <pavel@wheely.com>
2015-08-18 14:34:22 +0300

    Moved from/to AnyObject conversion to Foundation addition

--- Bunnyhop/Bunnyhop+Foundation.swift
+++ Bunnyhop/Bunnyhop+Foundation.swift
@@ -22,3 +22,76 @@ extension CGFloat: JSONDecodable, JSONEncodable {
         return .NumberValue(.DoubleValue(Double(self)))
     }
 }
+
+
+public extension JSON {
+    /// Converts from NSJSONSerialization's AnyObject
+    public static func fromAnyObject(anyObject: AnyObject?) -> JSON? {
+        switch anyObject {
+        
+        case let value as NSNull:
+            return .Nothing
+        
+        case let a as NSNumber:
+            switch CFNumberGetType(a as CFNumber) {
+            case .SInt8Type, .SInt16Type, .SInt32Type, .SInt64Type,
+                 .CharType, .ShortType, .IntType, .LongType, .LongLongType,
+                 .CFIndexType, .NSIntegerType:
+                return JSON(a as Int)
+            case .Float32Type, .Float64Type, .FloatType, .CGFloatType:
+                return JSON(a as Float)
+            case .DoubleType:
+                return JSON(a as Double)
+            }
+        
+        case let value as String:
+            return JSON(value)
+        
+        case let value as [AnyObject]:
+            var a = [JSON]()
+            for v in value {
+                if let v = JSON.fromAnyObject(v) {
+                    a.append(v)
+                } else {
+                    return nil // TODO: add strict and not strict version
+                }
+            }
+            return JSON(a)
+            
+        case let value as [String: AnyObject]:
+            var d: [String: JSON] = [:]
+            for (k, v) in value {
+                if let v = JSON.fromAnyObject(v) {
+                    d[k] = v
+                } else {
+                    return nil // TODO: add strict and not strict version
+                }
+            }
+            return JSON(d)
+            
+        default:
+            break
+        }
+        
+        return nil
+    }
+    
+    public func toAnyObject() -> AnyObject {
+        switch self {
+        case .Nothing:                          return NSNull()
+        case let .BoolValue(v):                 return v as NSNumber
+        case let .NumberValue(.IntValue(v)):    return v as NSNumber
+        case let .NumberValue(.FloatValue(v)):  return v as NSNumber
+        case let .NumberValue(.DoubleValue(v)): return v as NSNumber
+        case let .StringValue(v):               return v as NSString
+        case let .ArrayValue(v):                return v.map{$0.toAnyObject()} as NSArray
+        
+        case let .DictionaryValue(v):
+            var d: [String: AnyObject] = [:]
+            for (k, v) in v {
+                d[k] = v.toAnyObject()
+            }
+            return d
+        }
+    }
+}
